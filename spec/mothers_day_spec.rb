80ccba2fbc65daa50c0e7589a322985f89e5afb7
Daniel Seehausen <Daniel.Seehausen@gmail.com>
2018-06-07 12:25:49 -0400

    Update mothers_day_spec.rb

--- spec/mothers_day_spec.rb
+++ spec/mothers_day_spec.rb
@@ -1,14 +1,15 @@
 require_relative './spec_helper'
 
-describe "mothers_day" do
-  it 'defaults to Mom when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Happy mothers day, Mom! I hope you have a great day!")
-    mothers_day()
-  end
 
-  it 'accepts an argument of a name and prints out Hello with that Name' do
-    expect($stdout).to receive(:puts).with("Happy mothers day, Tracy! I hope you have a great day!")
+describe "mothers_day.rb" do
+
+  it 'accepts an argument of a name and prints out "Happy Mother\'s Day, <name>!"' do
+    expect(mothers_day("Tracy")).to eq("Happy Mother's Day, Tracy!")
     mothers_day("Tracy")
   end
-
+    
+  it 'defaults to Mom when no name is passed in' do
+    expect(mothers_day()).to eq("Happy Mother's Day, Mom!")
+  end
+  
 end
