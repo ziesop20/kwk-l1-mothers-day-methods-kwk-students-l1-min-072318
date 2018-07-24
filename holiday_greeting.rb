b9d3d1fc7dd586cd0b842335e6afc23e1eef9f1b
ziesop20 <sincerelysophie@hotmail.com>
2018-07-24 16:09:18 +0000

    automatically backed up by learn

--- holiday_greeting.rb
+++ holiday_greeting.rb
@@ -1,4 +1,4 @@
 def holiday_greeting (holiday= "Mother's Day", person ="Mom", friend= "Your Favorite Child")
-  puts "Happy #{holiday},#{person}!- From #{friend}"
+ "Happy #{holiday},#{person}!- From #{friend}"
 end 
-holiday_greeting ("Fourth of July" , "Beyonce" , "Jay-Z")
+puts holiday_greeting ("Fourth of July", "Beyonce" , "Jay-Z")
